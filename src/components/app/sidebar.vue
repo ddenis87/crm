0d0ca70afbaa6824cb07c4a909681b4f4b27c091
ddenis87 <dizdok@yandex.ru>
2020-06-02 10:29:16 +0900

    First commit

---
+++ src/components/app/sidebar.vue
@@ -0,0 +1,29 @@
+<template>
+  <ul class="sidenav app-sidenav" :class="{open: value}">
+    <router-link
+      v-for="sbRow in sidebar"
+      v-bind:key="sbRow.url" 
+      tag="li"
+      active-class="active"
+      :to="sbRow.url"
+      :exact="sbRow.exact"
+    >
+      <a href="#" class="waves-effect waves-orange pointer">{{sbRow.title}}</a>
+    </router-link>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: ["value"],
+  data: () => ({
+    sidebar: [
+      {url: "/", title: "Счет", exact: true},
+      {url: "/history", title: "История"},
+      {url: "/planning", title: "Планирование"},
+      {url: "/record", title: "Новая запись"},
+      {url: "/categories", title: "Категории"}
+    ]
+  })
+}
+</script>
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="app-main-layout">
+    <navbar v-on:hiddenNav="isOpen = !isOpen" />
+
+    <sidebar v-model="isOpen" />
+
+    <main class="app-content" :class="{full: !isOpen}">
+      <div class="app-page">
+        <router-view />
+      </div>
+    </main>
+
+    <div class="fixed-action-btn">
+      <a class="btn-floating btn-large blue" href="#">
+        <i class="large material-icons">add</i>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+import navbar from "@/components/app/navbar"
+import sidebar from "@/components/app/sidebar"
+export default {
+  data: () => ({
+    isOpen: true
+  }),
+  components: {
+    navbar, sidebar
+  }
+}
+</script>
